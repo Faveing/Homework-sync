d3392599e361bfffbd151120924c941683a41483
Faveing <MPearsonpear@protonmail.com>
2018-05-01 19:59:26 -0700

    new file:   Sync.sh

---
+++ Sync.sh
@@ -0,0 +1,7 @@
+cd ~/storage/shared/Documents/markor
+cp Homework ~/Homework-sync/Homework-sync/Homework
+cd ~/Homework-sync/Homework-sync/Homework
+git init
+git add .
+git commit -m "Homework"
+git push -u origin master
