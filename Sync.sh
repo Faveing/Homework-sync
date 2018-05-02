c2235f8acf3acff3457eb16983f61f08eb0f9c77
Faveing <MPearsonpear@protonmail.com>
2018-05-01 20:03:47 -0700

    Homework

--- Sync.sh
+++ Sync.sh
@@ -1,6 +1,6 @@
 cd ~/storage/shared/Documents/markor
 cp Homework ~/Homework-sync/Homework-sync/Homework
-cd ~/Homework-sync/Homework-sync/Homework
+cd ~/Homework-sync/Homework-sync
 git init
 git add .
 git commit -m "Homework"
